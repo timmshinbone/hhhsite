0e013dc9ca6177972854fe5e25b758f0e6dd6fb6
timmshinbone <timmschoenborn@gmail.com>
2026-08-21 12:41:42 -0500

    adjusts clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,13 +108,16 @@ export default function RootLayout({
       className={`${instrumentSerif.variable} ${raleway.variable} h-full antialiased`}
     >
       <head>
-        <Script id="clarity" strategy="afterInteractive">
-          {`(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window,document,"clarity","script","y4vpr5ftto");`}
-        </Script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y4vpr5ftto");`,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">
         <script
